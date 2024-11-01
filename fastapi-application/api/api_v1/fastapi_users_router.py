3c71f9134c02292531013a9025a664707f01a223
ami <h1mera@icloud.com>
2024-11-02 01:09:39 +0300

    move fastapi_users_router.py

--- fastapi-application/api/api_v1/fastapi_users_router.py
+++ fastapi-application/api/api_v1/fastapi_users_router.py
@@ -2,8 +2,8 @@ import uuid
 
 from fastapi_users import FastAPIUsers
 
-from .backend import authentication_backend
-from .user_manager import get_user_manager
+from api.dependencies.authentication.backend import authentication_backend
+from api.dependencies.authentication.user_manager import get_user_manager
 from core.models.user import User
 from core.types.user_id import UserIdType
 
