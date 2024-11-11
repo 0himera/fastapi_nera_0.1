440ac0b6a1db92a2e5163565e4ceac4a84132d95
ami <h1mera@icloud.com>
2024-11-11 14:46:06 +0300

    add cors

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
 
@@ -22,9 +23,25 @@ main_app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
+
+# need to move to config
+
+origins = [
+    "http://localhost:5173",
+]
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 main_app.include_router(
     api_router,
 )
+
 
 if __name__ == "__main__":
     uvicorn.run(
