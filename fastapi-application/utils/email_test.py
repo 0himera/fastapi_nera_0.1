d8002bbac5301ed6d0526cfdb0847276d11fe16e
ami <h1mera@icloud.com>
2024-11-20 12:33:47 +0300

    added test smtp

---
+++ fastapi-application/utils/email_test.py
@@ -0,0 +1,10 @@
+import smtplib
+
+server = smtplib.SMTP_SSL("smtp.mail.ru", 465)
+server.login("whychain@vk.com", "Fp2frTdepGJpR3eM0Syk")
+server.sendmail(
+    "whychain@vk.com",
+    "a_mir_m@mail.ru",
+    "Subject: Test Email\n\nThis is a test email."
+)
+server.quit()
