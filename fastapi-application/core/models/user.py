4f818154b9bec7607b00618cd04f66709bcfa7ba
ami <h1mera@icloud.com>
2024-11-29 22:02:25 +0300

    + relationship with feedback

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,8 +1,10 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, relationship
 
 from core.types.user_id import UserIdType
+from .feedback import Feedback
 
 if TYPE_CHECKING:
 	from sqlalchemy.ext.asyncio import AsyncSession
@@ -12,6 +14,7 @@ from .mixins.int_id_pk import IntIdPkMixin
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+	feedbacks: Mapped[List["Feedback"]] = relationship("Feedback", back_populates="user")
 
 	@classmethod
 	def get_db(cls, session: "AsyncSession"):
